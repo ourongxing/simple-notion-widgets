b8ece3adb06291cb05e7069509a4988e10de008f
ourongxing <orongxing@gmail.com>
2021-02-22 14:33:19 +0800

    update

--- video-player/main.js
+++ video-player/main.js
@@ -36,7 +36,8 @@ if (video.url) {
       .then(res => res.json())
       .then(res => res.data.picture)
       .then(res => {
-        video.pic = res.replace(/http/, "https") + "?time=" + Math.floor(Date.now() / 1000);
+        // video.pic = res.replace(/http/, "https") + "?time=" + Math.floor(Date.now() / 1000);
+        video.pic = res
       })
     await fetch("https://bili-api.vercel.app/api/v0/acg_video/list?bvid=" + video.bvid)
       .then(res => res.json())
