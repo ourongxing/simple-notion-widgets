11bd6fd914ab02d9ddc056c89497738e95d55bd8
ourongxing <orongxing@gmail.com>
2021-09-02 13:10:27 +0800

    update

--- video-player/main.js
+++ video-player/main.js
@@ -18,7 +18,8 @@ for (let i = 0; i < vars.length; i++) {
   }
   else paras[pair[0]] = pair[1];
 }
-if (video.url) {
+if (video.url && /.mp4/.test(video.url)) {
+  video.url.replace(/.mp4/,"")
   if (video.url.indexOf("?p") != -1) {
     video.bvid = video.url.split("video/")[1].split("?p=")[0];
     video.page = parseInt(video.url.split("video/")[1].split("?p=")[1]) - 1;
